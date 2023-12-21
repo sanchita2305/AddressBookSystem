0b95c447bf19a7e0a265f8f0b84228e2ce981cde
Sanchita More <69353328+sanchita2305@users.noreply.github.com>
2023-12-21 17:43:29 +0530

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,51 @@
+package AddressBookSystem;
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
+    public void updateContact(String address,String city,String state,String zip,String phoneNumber,String email){
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phoneNumber=phoneNumber;
+        this.email=email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
